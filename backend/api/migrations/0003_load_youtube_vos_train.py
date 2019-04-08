b227c7796afbcf0c980a154f77af6fd213100d18
Kevin Chen <kevinxchen13@gmail.com>
2019-04-08 15:33:13 -0400

    Fixed YouTube-VOS migration

--- backend/api/migrations/0003_load_youtube_vos_train.py
+++ backend/api/migrations/0003_load_youtube_vos_train.py
@@ -12,7 +12,7 @@ def load_youtube_vos_train(apps, schema_editor):
     Frame = apps.get_model("api", "Frame")
     FrameSegmentation = apps.get_model("api", "FrameSegmentation")
     SegmentedObject = apps.get_model("api", "SegmentedObject")
-    OcclusionFlag = apps.get_model("api", "OcclusionFlag")
+    # OcclusionFlag = apps.get_model("api", "OcclusionFlag")
     
     # Read meta.json
     with open(f"{base_dir}/meta.json") as f:
